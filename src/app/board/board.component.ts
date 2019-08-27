7c553aeadc7e6b56e36cf29932c7505c0f9becfc
Joseph Bauer <root@thesudorm.com>
2019-08-27 06:40:11 -0400

    Initial commit. Started to work on displaying the board.

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.drawHorizontalLines();
+  }
+
+  drawHorizontalLines() {
+
+  }
+}
